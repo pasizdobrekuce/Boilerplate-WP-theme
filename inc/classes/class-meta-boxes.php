a4d03eb1f29a9773b65681dfa128127bd2c0540d
Ratkovic Petar <ratkovicpetar@gmail.com>
2020-09-23 20:18:55 +0200

    Added meta Boxes functionality

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -5,43 +5,76 @@
  * @package Headlab
  */
 
- namespace HEADLAB_THEME\Inc;
+namespace HEADLAB_THEME\Inc;
 
 // Use Singleton
 use HEADLAB_THEME\Inc\Traits\Singleton;
 
+require_once('includes/class-metabox-constructor.php');
+
  class Meta_Boxes {
     use Singleton;
 
-     protected function __construct() {
-         $this->init();
-     }
+    protected $mtb;
 
-     protected function init() {
-          
-        //Init
-        add_action('add_meta_boxes', [$this, 'hdlb_add_custom_box']);
-     }
+    protected function __construct() {
 
-     public function hdlb_add_custom_box($post) {
-        $screens = ['post'];
-        foreach ($screens as $screen) {
-            add_meta_box(
-                'featured-post',           // Unique ID
-                __('Featured post', 'headlab'),  // Box title
-                [$this, 'featured_post_html'],  // Content callback, must be of type callable
-                $screen                   // Post type
-            );
+        // Init custom fields on home.php template
+        $this->add_mtb_to_home_template();
+    }
+
+    /**
+     * Add metaboxes to home template
+     * 
+     * @return void
+     */
+    private function add_mtb_to_home_template() {
+
+        if($this->get_template_filename() === 'home.php') {
+
+            $metabox = new Metabox_Constructor([
+                'id' => 'home_template_custom_fields',
+                'title' => __('Custom fields', 'headlab'),
+                'screen' => 'page'
+            ]);
+            
+            $metabox_repeater_block15_fields[] = $metabox->addImage(array(
+                'id' => 'metabox_repeater_image_field',
+                'label' => 'Block Image'
+            ), true);
+            
+            $metabox_repeater_block15_fields[] = $metabox->addText(array(
+                'id' => 'metabox_repeater_text_field',
+                'label' => 'Block Title'
+            ), true);
+            
+            $metabox_repeater_block15_fields[] = $metabox->addTextArea(array(
+                'id' => 'metabox_repeater_textarea_field',
+                'label' => 'Block Description'
+            ), true);
+            
+            $metabox->addRepeaterBlock(array(
+                'id' => 'metabox_repeater_block15',
+                'label' => 'Repeater Block Field',
+                'desc' => 'Repeater Description.',
+                'fields' => $metabox_repeater_block15_fields,
+                'single_label' => 'Block'
+            ));
+            
+            $metabox_repeater_block15_fields[] = $metabox->addWysiwyg(array(
+                'id' => 'metabox_wysiwyg_field',
+                'label' => 'WYSIWYG Field',
+                'desc' => 'WYSIWIG Description.'
+            ));
         }
-     }
+    }
 
-     public function featured_post_html($post) {
-        $value = get_post_meta($post->ID, '_featured_post', true);
-        ?>
-        <label for="hdlb_field">
-            <?php esc_html_e( 'Is this a featured post?', 'headlab' ); ?>
-        </label>
-        <input type="checkbox" name="hdlb_field" id="hdlb-field" value="1" <?php checked( $value, 1 ); ?> /> <small>Yes/No</small>
-        <?php
-     }
+    private function get_template_filename() {
+
+        $post_id = $_REQUEST['post'] ?? null;
+        
+        $pageTemplate = basename(get_post_meta($post_id, '_wp_page_template', true));
+
+        return $pageTemplate; // {template_name}.php
+    }
  }
---
+++ page-templates/home.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Template name: Home
+ * 
+ * @package Headlab
+ */
+?>
+
+<?php get_header(); ?>
+
+front page 
+
+<?php get_footer(); ?>
